734cada535051fec7ae3e1d014589e531ecfd784
codinglone <codinglone@gmail.com>
2023-07-24 12:47:46 +0200

    UI changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { Button } from "primereact/button";
 import 'primeicons/primeicons.css';
-
 const Navbar = () => {
   return (
     <Nav>
