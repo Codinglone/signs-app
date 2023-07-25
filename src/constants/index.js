41210f93c3e3bd21332c7e8b7fae542fd688496e
codinglone <codinglone@gmail.com>
2023-07-25 10:00:56 +0200

    Added numbers images

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+import one from "../assets/numbers/1.jpeg"
+import two from "../assets/numbers/2.jpeg"
+import three from "../assets/numbers/3.jpeg"
+import four from "../assets/numbers/4.jpeg"
+import five from "../assets/numbers/5.jpeg"
+import six from "../assets/numbers/6.jpeg"
+import seven from "../assets/numbers/7.jpeg"
+import eight from "../assets/numbers/8.jpeg"
+import nine from "../assets/numbers/9.jpeg"
+
+
+export { one, two, three, four, five, six, seven, eight, nine }
