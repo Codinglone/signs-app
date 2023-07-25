f8be9486e23dd57958ee04253eed6eb56e65f4a1
codinglone <codinglone@gmail.com>
2023-07-25 09:39:08 +0200

    Added textarea to sandbox

--- src/components/Sandbox.jsx
+++ src/components/Sandbox.jsx
@@ -1,7 +1,20 @@
-const Sandbox = () => {
-  return (
-    <div>Sandbox</div>
-  )
-}
+import { useState } from "react";
+import { InputTextarea } from "primereact/inputtextarea";
 
-export default Sandbox
+const Sandbox = () => {
+  const [value, setValue] = useState('')
+  return (
+    <div>
+      <h1>Enter a word or phrase</h1>
+      <InputTextarea
+        autoResize
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        rows={5}
+        cols={30}
+      />
+    </div>
+  );
+};
+
+export default Sandbox;
